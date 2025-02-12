1090933e69c32ee5a91a38247ddaf45662263ca7
tushar885 <tushar8104@gmail.com>
2025-02-12 13:03:04 +0530

    updated code as per review comments

--- main.c
+++ main.c
@@ -9,23 +9,19 @@ struct Node
 
 int isDigit(char ch)
 {
-    if (ch >= '0' && ch <= '9')
-    {
-        return 1;
-    }
-    return 0;
+    return (ch >= '0' && ch <= '9');
 }
 
 void inputStringToArray(int **array, int *size)
 {
-    char str[1000];
-    scanf("%[^\n]%*c", str);
+    char string[1000];
+    scanf("%[^\n]%*c", string);
     int i = 0;
     *size = 0;
 
-    while (str[i] != '\0')
+    while (string[i] != '\0')
     {
-        if (str[i] == ' ')
+        if (string[i] == ' ')
         {
             (*size)++;
             i++;
@@ -36,23 +32,28 @@ void inputStringToArray(int **array, int *size)
     *size = *size + 1;
 
     *array = malloc(*size * sizeof(int));
+    if (array == NULL)
+    {
+        printf("Error in memory allocaiton");
+        return;
+    }
     int index = 0;
     i = 0;
 
-    while (str[i] != '\0')
+    while (string[i] != '\0')
     {
         int sign = 1;
-        if (str[i] == '-')
+        if (string[i] == '-')
         {
             sign = -1;
             i++;
         }
-        if (isDigit(str[i]))
+        if (isDigit(string[i]))
         {
             int num = 0;
-            while (isDigit(str[i]))
+            while (isDigit(string[i]))
             {
-                num = num * 10 + (str[i] - '0');
+                num = num * 10 + (string[i] - '0');
                 i++;
             }
             (*array)[index++] = num * sign;
@@ -67,6 +68,11 @@ void inputStringToArray(int **array, int *size)
 struct Node *createNode(int val)
 {
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    if (newNode == NULL)
+    {
+        printf("Error in memory allocaiton");
+        return newNode;
+    }
     newNode->val = val;
     newNode->next = NULL;
     return newNode;
@@ -166,11 +172,10 @@ struct Node *quickSort(struct Node *head)
 void printLinkedList(struct Node *head)
 {
 
-    struct Node *temp = head;
-    while (temp != NULL)
+    while (head != NULL)
     {
-        printf("%d ", temp->val);
-        temp = temp->next;
+        printf("%d ", head->val);
+        head = head->next;
     }
 }
 
@@ -184,6 +189,7 @@ void freeLinkedList(struct Node *head)
         temp = head;
         head = head->next;
         free(temp);
+        temp = NULL;
     }
 }
 
